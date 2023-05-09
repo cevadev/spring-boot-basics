02630d0c0e6dd3844d2ad35e624bb625ee9d450a
cevadev <carlos.villanueva.altuna@gmail.com>
2023-05-08 21:00:55 -0300

    Java Spring Boot Basics

--- src/main/java/com/ceva/Main.java
+++ src/main/java/com/ceva/Main.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -14,7 +17,45 @@ public class Main {
 
     // exponemos un endpoint de tipo get
     @GetMapping("/greet")
-    public String geet(){
-        return "Hello guys...";
+    public GreetResponse geet(){
+        GreetResponse response = new GreetResponse("Hello",
+                List.of("Java", "PHP", "Javascript"), new Person("John", 30, 5000));
+        return response;
+    }
+
+    record Person(String name, int age, double chash){}
+    record GreetResponse(String greet, List<String> favProgrammingLanguages, Person person){}
+}
+/*
+class GreetResponse{
+    private final String greet;
+
+    public GreetResponse(String greet){
+        this.greet = greet;
+    }
+
+    public String getGreet(){
+        return greet;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GreetResponse that = (GreetResponse) o;
+        return Objects.equals(greet, that.greet);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(greet);
+    }
+
+    @Override
+    public String toString() {
+        return "GreetResponse{" +
+                "greet='" + greet + '\'' +
+                '}';
     }
 }
+*/
